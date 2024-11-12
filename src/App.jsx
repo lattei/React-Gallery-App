0adeb5a8db0f6f05b91326d89d71351ac86a44bf
Carline Senat <carlinesenat@gmail.com>
2024-11-12 14:24:07 -0800

    imported axios
    Created components and figuring out what to complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import apiKey from './config'
+import React, { useState, useEffect } from 'react'
+import apiKey from './config';
+import axios from 'axios';
+
+//App components
+import Search from './components/Search';
+import Nav from './components/Nav';
+import Photo from './components/Photo';
+import PhotoList from './components/PhotoList';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [photos, setPhotos] = useState([])
+
+  const fetchData = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        setPhotos(response.data.photos.photo);
+      })
+      .catch(error =>  {
+        console.log(error, "There's been an error getting your photos..");
+      }); 
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <Search onSearch={fetchData} />
+      <Nav />
+      <Routes>
+        
+      </Routes>
+
+    </div>
+  );
 }
 
 export default App
